84b114f7ce257fdbb6eed06b0856a733b57354cf
jackpoz <giacomopoz@gmail.com>
2020-10-18 13:24:26 +0200

    Allow to edit step size from UI

--- RecastDemo/Source/Sample.cpp
+++ RecastDemo/Source/Sample.cpp
@@ -245,6 +245,7 @@ void Sample::handleCommonSettings()
 	imguiSlider("Max Climb", &m_agentMaxClimb, 0.1f, 5.0f, 0.1f);
 	imguiSlider("Max Slope", &m_agentMaxSlope, 0.0f, 90.0f, 1.0f);
 	imguiSlider("Max Slope Not Steep", &m_agentMaxSlopeNotSteep, 0.0f, 90.0f, 1.0f);
+	imguiSlider("Step Size", &m_stepSize, 0.5f, 4.0f, 0.1f);
 	
 	imguiSeparator();
 	imguiLabel("Region");
